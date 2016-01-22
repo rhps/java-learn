709b1377afb16fe50e5dd9ee0ac09d0c2306a64e
Rio Harapan <riioharapan@gmail.com>
2016-01-23 01:28:19 +0700

    initial commit

---
+++ ch1.3/Flip.java
@@ -0,0 +1,17 @@
+public class Flip 
+{
+	public static void main(String[] args)
+	{
+		double t = Math.random();
+		if (t < 0.5)
+		{
+			System.out.println("Heads");
+			System.out.println(t);
+		}
+		else
+		{
+			System.out.println("Tails");
+                        System.out.println(t);
+		}
+	}
+}
---
+++ ch1.3/Gambler.java
@@ -0,0 +1,5 @@
+public class Gambler
+{
+	public static void main(String[] args)
+	
+}
